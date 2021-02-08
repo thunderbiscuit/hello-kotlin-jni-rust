348dc5282ceddeaedca4bf7b1a9c57ce12994b23
thunderbiscuit <thunderB@protonmail.com>
2021-02-08 17:57:02 -0500

    Add usage of native Rust functions

--- kotlin/calculator/src/main/kotlin/org/acmecalculators/calculator/main.kt
+++ kotlin/calculator/src/main/kotlin/org/acmecalculators/calculator/main.kt
@@ -1,5 +1,22 @@
 package org.acmecalculators.calculator
 
 fun main() {
-    println("Hello, World!")
+    
+    val a: Int = 19
+    val b: Int = 2
+
+    // create calculator object and test local Kotlin add() method
+    val calculator: Calculator = Calculator()
+    calculator.add(a, b)
+    
+    // load native library
+    Calculator.load()
+
+    // use Rust native method addNative()
+    val result: Int = calculator.nativeAdd(a, b)
+    println("You added $a and $b using the native calculator; your result is $result")
+
+    // use Rust native method helloNative()
+    val message: String = calculator.nativeHello("Kotlin")
+    println(message)
 }
